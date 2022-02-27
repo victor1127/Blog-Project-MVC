0ad878502965b431b5c706c32a013c9c96a5d592
Victor Sosa <vsosa01127@gmail.com>
2022-02-27 12:23:44 -0400

    Add project files.

---
+++ Configurations/BlogConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using BlogProjectMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProjectMVC.Configurations
+{
+    public class BlogConfiguration
+    {
+        public BlogConfiguration(ModelBuilder builder)
+        {
+            builder.Entity<Blog>()
+                   .HasKey(k => k.Id);
+
+            builder.Entity<Blog>()
+                   .Property(t => t.Title)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Entity<Blog>()
+                   .Property(d => d.Description)
+                   .IsRequired()
+                   .HasMaxLength(500);
+
+            builder.Entity<Blog>()
+                   .Ignore(i => i.ImageFile);
+
+        }
+    }
+}
---
+++ Models/Blog.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProjectMVC.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+        [Required]
+        [StringLength(100,ErrorMessage ="The {0} must be at least {2} and at most {1}", MinimumLength =2)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1}", MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Created date")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated date")]
+        public DateTime? Updated { get; set; }
+        public byte[] ImageData { get; set; }
+        public string ImageType { get; set; }
+
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+
+    }
+
+
+}
